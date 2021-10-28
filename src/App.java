fad078587bd195bba08e21935d1ce0075cb43115
0TrashPanda <jonah.jvdb@gmail.com>
2021-10-28 14:10:10 +0200

    added color to treeview

--- src/App.java
+++ src/App.java
@@ -1,11 +1,18 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
+// import javafx.stage.StageStyle;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class App extends Application {
 
@@ -42,8 +49,8 @@ public class App extends Application {
         rootItem.getChildren().add(customgates);
 
         TreeView<String> treeView = new TreeView<String>();
-        canvas.getStyleClass().add("treeView");
-
+        canvas.getStyleClass().add("canvas");
+        treeView.getStyleClass().add("TreeView");
         treeView.setRoot(rootItem);
         DragResizerXY.makeResizable(treeView);
         parent.getChildren().add(treeView);
@@ -54,8 +61,28 @@ public class App extends Application {
         Scene scene = new Scene(parent);
         String css = this.getClass().getResource("main.css").toExternalForm();
         scene.getStylesheets().add(css);
+        // stage.initStyle(StageStyle.TRANSPARENT);
+
+        Circle circle = new Circle(100, 100, 100);
+        circle.getStyleClass().add("circle");
+        canvas.getChildren().add(circle);
+        circle.setFill(Color.LIMEGREEN);
+
+        circle.setOnDragDetected((MouseEvent event) -> {
+            System.out.println("Circle 1 drag detected");
+
+            Dragboard db = circle.startDragAndDrop(TransferMode.ANY);
+
+            ClipboardContent content = new ClipboardContent();
+            content.putString("Circle source text");
+            db.setContent(content);
+        });
+        circle.setOnMouseDragged((MouseEvent event) -> {
+            event.setDragDetect(true);
+        });
 
         stage.setScene(scene);
         stage.show();
     }
+
 }
