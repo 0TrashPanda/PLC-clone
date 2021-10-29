04743bfda33ac6bd96f495927d6ffadcc572d85d
0TrashPanda <jonah.jvdb@gmail.com>
2021-10-29 15:36:52 +0200

    troep verwijderd

--- src/App.java
+++ src/App.java
@@ -49,7 +49,7 @@ public class App extends Application {
         stage.setMaximized(true);
         stage.setMinHeight(250);
         stage.setMinWidth(500);
-        
+
         HBox parent = new HBox();
         Pane canvas = new Pane();
         canvas.prefWidthProperty().bind(parent.widthProperty());
@@ -72,7 +72,7 @@ public class App extends Application {
                 depNode.getChildren().add(empLeaf);
             }
         }
-        
+
         stage.setTitle("Tree View Sample");
         VBox box = new VBox();
         parent.getChildren().add(box);
@@ -136,73 +136,6 @@ public class App extends Application {
                 event.consume();
             }
         });
-
-        // private final class TextFieldTreeCellImpl extends TreeCell<String> {
-
-        // private TextField textField;
-
-        // public TextFieldTreeCellImpl() {
-        // }
-
-        // @Override
-        // public void startEdit() {
-        // super.startEdit();
-
-        // if (textField == null) {
-        // createTextField();
-        // }
-        // setText(null);
-        // setGraphic(textField);
-        // textField.selectAll();
-        // }
-
-        // @Override
-        // public void cancelEdit() {
-        // super.cancelEdit();
-        // setText((String) getItem());
-        // setGraphic(getTreeItem().getGraphic());
-        // }
-
-        // @Override
-        // public void updateItem(String item, boolean empty) {
-        // super.updateItem(item, empty);
-
-        // if (empty) {
-        // setText(null);
-        // setGraphic(null);
-        // } else {
-        // if (isEditing()) {
-        // if (textField != null) {
-        // textField.setText(getString());
-        // }
-        // setText(null);
-        // setGraphic(textField);
-        // } else {
-        // setText(getString());
-        // setGraphic(getTreeItem().getGraphic());
-        // }
-        // }
-        // }
-
-        // private void createTextField() {
-        // textField = new TextField(getString());
-        // textField.setOnKeyReleased(new EventHandler<KeyEvent>() {
-
-        // @Override
-        // public void handle(KeyEvent t) {
-        // if (t.getCode() == KeyCode.ENTER) {
-        // commitEdit(textField.getText());
-        // } else if (t.getCode() == KeyCode.ESCAPE) {
-        // cancelEdit();
-        // }
-        // }
-        // });
-        // }
-
-        // private String getString() {
-        // return getItem() == null ? "" : getItem().toString();
-        // }
-        // }
 
         canvas.getChildren().add(CircleGroup);
 
